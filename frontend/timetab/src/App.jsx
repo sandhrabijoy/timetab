abc7e4e1673298900a270f169206d046a3c81282
sandhra <sandhrasonabijoy@gmail.com>
2025-01-27 09:34:45 +0900

    added the small heading

--- frontend/timetab/src/App.jsx
+++ frontend/timetab/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Button from './atoms/Button/Button';
 import Heading from './atoms/Heading/Heading';
 import Dropdown from './atoms/dropdown/Dropdown';
-
+import Smallhead from './atoms/Smallhead/Smallhead';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +20,8 @@ function App() {
         
         <Heading className="big-heading"></Heading>
         <Heading className="small-heading"></Heading>
+        <Smallhead className="b-heading"></Smallhead>
+        <Smallhead className="s-heading"></Smallhead>
               </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ frontend/timetab/src/atoms/Smallhead/Smallhead.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Smallhead.css"
+const Smallhead = (props) => {
+  return (
+    <div className={props.className}>Next Tech Education</div>
+  )
+}
+export default Smallhead
